0aac5b3ca8d54d5572ccf17c4222a718660f0282
Jason Cowin <jcowin@sigmadzn.com>
2023-12-13 11:44:49 -0800

    Completed q3
    - It is a little hard to follow the code
    - Passed tests online

---
+++ q3_longest_substring_without_repeating_characters/Program.cs
@@ -0,0 +1,44 @@
+﻿
+var solution = new Solution();
+
+
+List<string> strings = ["abcabcabc",
+                        "aaaaaaaaa",
+                        "abbcdefgh"];
+
+foreach(var s in strings){
+    int ans = solution.LengthOfLongestSubstring(s);
+    Console.WriteLine($"{ans} - {s}");
+}
+
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+
+        int longest = 0;
+
+        int startIndex = 0;
+        int length = 0;
+
+        while(startIndex + length <= s.Length){
+            var sub = s.Substring(startIndex, length);
+
+            length = sub.Length;
+            longest = longest >= length ? longest : length;
+            
+            // Try to add another
+            if(s.Length == startIndex + length)
+                break;
+            
+            char nextChar = s[startIndex+length];
+
+            while(sub.Contains(nextChar)){
+                sub = s.Substring(++startIndex, --length);
+            }
+
+            length++;
+        }
+
+
+        return longest;
+    }
+}
